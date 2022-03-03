279901752eb83d597ac1bc3b5848683704e09b29
hrmdbipolar <hrmandr@gmail.com>
2022-03-02 21:07:19 -0800

    no-qrCode

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(private authService: AuthService){}
+  
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('user-info')
+  getUserInfo(@Request() req) {
+    return req.user
   }
 }
