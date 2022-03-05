e2674488a21e8c8a46ad82071479fdab4b83710c
hrmdbipolar <65605498+hrmdbipolar@users.noreply.github.com>
2022-03-05 09:35:12 +0000

    Revert "Authentication"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthenticationModule } from './authentication/authentication.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthenticationModule, UsersModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
