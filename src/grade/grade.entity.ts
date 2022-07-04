d38635b04f618d071f1727d716b6fe3ef6b8aa6e
hrmdbipolar <hrmandr@gmail.com>
2022-07-04 12:48:02 +0200

    entities

---
+++ src/grade/grade.entity.ts
@@ -0,0 +1,18 @@
+import { BaseEntity } from "src/shared/entities/base.entity";
+import { StudentEntity } from "src/student/student.entity";
+import { SubjectEntity } from "src/subject/subject.entity";
+import { Column, Entity, JoinTable, OneToMany } from "typeorm";
+
+@Entity({ name: 'grade' })
+export class GradeEntity extends BaseEntity {
+    @Column({ name: 'name', nullable: false, type: 'text' })
+    public name: string;
+
+    @OneToMany(() => StudentEntity, (student => student.id), { cascade: true })
+    @JoinTable()
+    students: StudentEntity[];
+
+    @OneToMany(() => SubjectEntity, (subject => subject.id), { cascade: true })
+    @JoinTable()
+    subjects: SubjectEntity[];
+}
--- src/subject/subject.entity.ts
+++ src/subject/subject.entity.ts
@@ -1,5 +1,7 @@
+import { GradeEntity } from "src/grade/grade.entity";
 import { BaseEntity } from "src/shared/entities/base.entity";
-import { Column, Entity } from "typeorm";
+import { UserEntity } from "src/user/user.entity";
+import { Column, Entity, JoinColumn, OneToMany, OneToOne } from "typeorm";
 
 @Entity({ name: 'subject' })
 export class SubjectEntity extends BaseEntity {
@@ -9,4 +11,14 @@ export class SubjectEntity extends BaseEntity {
     @Column({ name: 'grade_id', nullable: true, type: 'text' })
     public gradeId: string;
 
+    @Column({ name: 'teacher_id', nullable: false, type: 'text' })
+    public teacherId: string;
+
+    @OneToOne(() => UserEntity)
+    @JoinColumn({ name: 'teacher_id' })
+    user: UserEntity;
+
+    @OneToOne(() => GradeEntity)
+    @JoinColumn({ name: 'grade_id' })
+    grade: GradeEntity;
 }
